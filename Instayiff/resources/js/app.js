e7e05e10009e30efd674bd24ead5c00873f9c282
meleeman01 <meleeman01@live.com>
2020-05-13 07:09:14 -0500

    updated roadmap, and added an example react component to get started.

--- Instayiff/resources/js/app.js
+++ Instayiff/resources/js/app.js
@@ -1,15 +1,6 @@
-//main 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './exampleProject';
 
-function App() {
-    console.log('ayyyyye');
-    const firstName = 'bob';
-    const lastName = 'dankman';
-
-    return (<h1>Hello {`${firstName} ${lastName}`}!</h1>);
-    
-    
-}
 
 ReactDOM.render(<App />,document.getElementById('root'));
